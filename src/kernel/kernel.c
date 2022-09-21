f4355ea489e656fb188d7215e406e3f5cf3f8271
I514692 <klose.wu@sap.com>
2022-09-21 16:50:47 +0800

    print to a real screen with framebuffer

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,22 +1,30 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <kernel/uart.h>
+#include <kernel/mem.h>
+#include <kernel/atag.h>
+#include <kernel/gpu.h>
 #include <common/stdio.h>
 #include <common/stdlib.h>
 
+
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 {
-        char buf[256];
-        // Declare as unused
-        (void) r0;
-        (void) r1;
-        (void) atags;
+    char buf[256];
+    // Declare as unused
+    (void) r0;
+    (void) r1;
+    (void) atags;
 
-        uart_init();
-        puts("Hello, kernel World!\n");
-        while (1) {
-                gets(buf,256);
-                puts(buf);
-                putc('\n');
-        }
+    puts("Initializing Memory Module\n");
+    mem_init((atag_t *)atags);
+    gpu_init();
+
+    puts("Hello, kernel World!\n");
+
+    while (1) {
+        gets(buf,256);
+        puts(buf);
+        putc('\n');
+    }
 }
